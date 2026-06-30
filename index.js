4c4f1e69c5608dbac13c73c5fc61dc1b3f074bbb
heroscha1992 <41587907+heroscha1992@users.noreply.github.com>
2026-06-30 19:00:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// middleware عشان نقرأ JSON
+app.use(express.json());
+
+// بيانات وهمية
+let users = [
+  { id: 1, name: "Ali" },
+  { id: 2, name: "Sara" }
+];
+
+// الصفحة الرئيسية
 app.get("/", (req, res) => {
   res.send("API is working 🚀");
 });
 
-app.get("/health", (req, res) => {
-  res.send("OK");
+// جلب كل المستخدمين
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// إضافة مستخدم
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name
+  };
+  users.push(newUser);
+  res.json(newUser);
 });
 
 app.listen(PORT, "0.0.0.0", () => {
